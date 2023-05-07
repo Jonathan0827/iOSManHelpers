7b5e8d49a331e45994f8567f05a07e3d3576edbe
Jun Hyeop <limjunehyeop@gmail.com>
2023-05-07 21:21:27 +0900

    Update

--- getUserData.sh
+++ getUserData.sh
@@ -1,4 +1,4 @@
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/users/USERNAME/hovercardi
+  https://api.github.com/users/"$1"/hovercardi
