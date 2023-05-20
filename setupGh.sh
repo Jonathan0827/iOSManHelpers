ea72c9a3f01d591115b3da39601d5a9fc6664cb2
Jun Hyeop <limjunehyeop@gmail.com>
2023-05-20 22:41:49 +0900

    made big changes

--- setupGh.sh
+++ setupGh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 open -a Terminal
 osascript \
-    -e "tell application \"Terminal\"" \
-    -e "do script \"exit\" in tab 1 of front window" \
-    -e "end tell" > /dev/null
+  -e "tell application \"Terminal\"" \
+  -e "do script \"gh auth login -p https -w && exit\" in tab 1 of front window" \
+  -e "end tell"
+#osascript \
+#    -e "quit app \"Terminal\""
