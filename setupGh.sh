0c671d23ca562877a688ed9d9b9b074312f46ae6
임준협 <jonathanlim@imjunhyeob-ui-MacBookAir.local>
2023-07-08 14:45:20 +0900

    addBrew

--- setupGh.sh
+++ setupGh.sh
@@ -2,7 +2,7 @@
 open -a Terminal
 osascript \
   -e "tell application \"Terminal\"" \
-  -e "do script \"gh auth login -p https -w && exit\" in tab 1 of front window" \
+  -e "do script \"sleep 1 && gh auth login -p https -w && exit\" in tab 1 of front window" \
   -e "end tell"
 #osascript \
 #    -e "quit app \"Terminal\""
